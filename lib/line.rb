01e8b08a997b9b6738a90b7cbc852fb8df6221ba
geh4y8 <guy.halperin88@gmail.com>
2014-08-13 21:10:45 -0700

    add save line functionality

--- lib/line.rb
+++ lib/line.rb
@@ -6,4 +6,22 @@ class Line
     @name = line_info[:name]
     @id = line_info[:id]
   end
+
+  def save
+    result = DB.exec("INSERT INTO lines (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def self.all
+    lines =[]
+    results = DB.exec("SELECT * FROM lines;")
+    results.each do |result|
+      lines<< Line.new({:name => result['name'], :id => result['id']})
+    end
+    lines
+  end
+
+  def ==(another_line)
+    self.name == another_line.name
+  end
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -5,4 +5,26 @@ describe Line do
     new_line = Line.new({:name => 'US EXPRESS'})
     expect(new_line).to be_a Line
   end
+  
+  it 'saves a line to the database' do
+    new_line = Line.new({:name => 'US EXPRESS'})
+    new_line.save
+    expect(Line.all).to eq [new_line]
+  end
+
+  it 'lists all lines in the database' do
+    new_line = Line.new({:name => 'US EXPRESS'})
+    new_line.save
+    new_line2 = Line.new({:name => 'CANADA EXPRESS'})
+    new_line2.save
+    expect(Line.all).to eq [new_line, new_line2]
+  end
+
+  it 'recognizes when two lines are equal' do
+    new_line = Line.new({:name => 'US EXPRESS'})
+    new_line.save
+    new_line2 = Line.new({:name => 'US EXPRESS'})
+    new_line2.save
+    expect(new_line).to eq new_line2
+  end
 end
