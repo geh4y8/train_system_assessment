3a5a8cd10ac0dc784e9989602d4f1f9abbdaa1fa
geh4y8 <guy.halperin88@gmail.com>
2014-08-13 21:30:55 -0700

    save station

--- lib/station.rb
+++ lib/station.rb
@@ -6,4 +6,22 @@ class Station
     @name = station_info[:name]
     @id = station_info[:id]
   end
+
+  def save
+    result = DB.exec("INSERT INTO stations (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def self.all
+    stations =[]
+    results = DB.exec("SELECT * FROM stations;")
+    results.each do |result|
+      stations<< Station.new({:name => result['name'], :id => result['id']})
+    end
+    stations
+  end
+
+  def ==(another_station)
+    self.name == another_station.name
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,4 +5,26 @@ describe Station do
     new_station = Station.new({:name => 'north'})
     expect(new_station).to be_a Station
   end
+
+  it 'saves a line to the database' do
+    new_station = Station.new({:name => 'north'})
+    new_station.save
+    expect(Station.all).to eq [new_station]
+  end
+
+  it 'lists all lines in the database' do
+    new_station = Station.new({:name => 'north'})
+    new_station.save
+    new_station2 = Station.new({:name => 'south'})
+    new_station2.save
+    expect(Station.all).to eq [new_station, new_station2]
+  end
+
+  it 'recognizes when two lines are equal' do
+    new_station = Station.new({:name => 'north'})
+    new_station.save
+    new_station2 = Station.new({:name => 'north'})
+    new_station2.save
+    expect(new_station).to eq new_station2
+  end
 end
