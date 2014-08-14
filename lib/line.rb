c34f55aadc1f1f40d653209755364e1ddfdb2892
geh4y8 <guy.halperin88@gmail.com>
2014-08-13 21:34:05 -0700

    list lines for stops

--- lib/line.rb
+++ lib/line.rb
@@ -24,4 +24,30 @@ class Line
   def ==(another_line)
     self.name == another_line.name
   end
+
+  def self.find_line(line_name)
+    line = []
+    results = DB.exec("SELECT * FROM lines WHERE name = '#{line_name}';")
+    results.each do |result|
+      line << Line.new({:name => result['name'], :id => result['id']})
+    end
+    line.first
+  end
+
+  def add_station(input_station_id)
+    DB.exec("INSERT INTO stops (station_id, line_id) VALUES (#{input_station_id} , #{self.id});")
+  end
+
+  def self.line_list(input_station_id)
+    lines = []
+    results = DB.exec("SELECT lines.* FROM
+              stations join stops on (stations.id = stops.station_id)
+                join lines on (stops.line_id = lines.id)
+              where stations.id = '#{input_station_id}';")
+    results.each do |result|
+      lines << Line.new({:id => result['id'], :name => result['name']})
+    end
+    lines
+  end
+end
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -5,7 +5,7 @@ describe Line do
     new_line = Line.new({:name => 'US EXPRESS'})
     expect(new_line).to be_a Line
   end
-  
+
   it 'saves a line to the database' do
     new_line = Line.new({:name => 'US EXPRESS'})
     new_line.save
@@ -26,5 +26,17 @@ describe Line do
     new_line2 = Line.new({:name => 'US EXPRESS'})
     new_line2.save
     expect(new_line).to eq new_line2
+  end
+
+  it 'lists all lines for a given stop' do
+    new_line = Line.new({:name => 'US EXPRESS'})
+    new_line.save
+    new_line2 = Line.new({:name => 'CANADA EXPRESS'})
+    new_line2.save
+    new_station = Station.new({:name => 'north'})
+    new_station.save
+    new_line.add_station(new_station.id)
+    new_line2.add_station(new_station.id)
+    expect(Line.line_list(new_station.id)).to eq [new_line, new_line2]
   end
 end
